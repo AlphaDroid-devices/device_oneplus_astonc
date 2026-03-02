AlphaDroid-devices/device_oneplus_aston

AlphaDroid bringup

AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_aston.mk
+    $(LOCAL_DIR)/alpha_aston.mk
+
+COMMON_LUNCH_CHOICES := \
+    alpha_aston-user \
+    alpha_aston-userdebug \
+    alpha_aston-eng

AlphaDroid-devices/device_oneplus_astonc

Change-Id: I34af68fea86ee1f92da9c0264830f0ed7e8312da

AndroidProducts.mk
@@ -4,4 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_astonc.mk
+    $(LOCAL_DIR)/alpha_astonc.mk
+
+COMMON_LUNCH_CHOICES := \
+    alpha_astonc-user \
+    alpha_astonc-userdebug \
+    alpha_astonc-eng