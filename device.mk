bf869d657be29a9650ffcd89be6fefeda660e1c2
elpaablo <donopwhile1@gmail.com>
2026-07-24 00:19:34 +0100

    Adapt power mode extension for power-libperfmgr

--- device.mk
+++ device.mk
@@ -61,7 +61,7 @@ PRODUCT_PACKAGES += \
     OPlusWifiResTarget
 
 # Power
-$(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,power-ext-oplus)
 
 # Sensors
 PRODUCT_PACKAGES += \
